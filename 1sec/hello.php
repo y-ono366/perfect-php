9d6a694594c2fd713e1e7af7e0e9e20aca133ea9
yusuke ohno <yusuke@ohno.systems>
2019-04-30 20:19:02 +0900

    add 1sec

---
+++ 1sec/hello.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html PUBLIC "-//W3d//DTD XHTML 1.0 Trasitional //EN"
+"html://www.w3.org/TR/xhtml1/DTD/xhtml1-transitiona.dtd">
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <title>Hello,World</title>
+</head>
+<body>
+    <p><?php echo "Hello, I'm a PHP!";?></p>
+</body>
+</html>
